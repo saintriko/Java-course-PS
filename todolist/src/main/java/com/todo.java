11882450a979acb76e95d9d8058150110d6e758c
Maks Semeev <makssemeev@gmail.com>
2020-03-01 18:35:12 +0300

    init todolist project

---
+++ todolist/src/main/java/com/todo.java
@@ -0,0 +1,12 @@
+package com;
+
+import java.util.Scanner;
+
+public class todo {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        String s = keyboard.nextLine();
+        System.out.println(s);
+
+    }
+}
